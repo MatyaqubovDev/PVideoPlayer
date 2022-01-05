b833e97019a46f06af7b4fba0a4021fdbbfe9cdc
Bogibek_Matyaqubov <uzroot.pdp@gmail.com>
2022-01-05 12:17:31 +0500

    Player Ready with picker

--- app/src/main/java/com/example/pvideoplayer/MainActivity.kt
+++ app/src/main/java/com/example/pvideoplayer/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.pvideoplayer
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.VideoView
 import android.widget.ViewFlipper
 import com.khizar1556.mkvideoplayer.MKPlayerActivity
 import net.alhazmy13.mediapicker.Video.VideoPicker
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var videoPlayer: VideoView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,14 +21,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        val playVideo=findViewById<Button>(R.id.btn_play_video)
+        val playVideo = findViewById<Button>(R.id.btn_play_video)
+        videoPlayer = findViewById<VideoView>(R.id.video_player)
         playVideo.setOnClickListener {
             pickVideo()
         }
     }
 
     private fun pickVideo() {
-        val videoPicker=VideoPicker.Builder(this@MainActivity)
+        val videoPicker = VideoPicker.Builder(this@MainActivity)
             .mode(VideoPicker.Mode.GALLERY)
             .directory(VideoPicker.Directory.DEFAULT)
             .extension(VideoPicker.Extension.MP4)
@@ -35,10 +39,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode==VideoPicker.VIDEO_PICKER_REQUEST_CODE && resultCode== RESULT_OK){
-            val videoPaths=data!!.getStringArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH)
-            Log.d("videoPaths","OnActivity: $videoPaths")
-            MKPlayerActivity.configPlayer(this@MainActivity).play(videoPaths!![0])
+        if (requestCode == VideoPicker.VIDEO_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            val videoPaths = data!!.getStringArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH)
+            //MKPlayerActivity.configPlayer(this@MainActivity).play(videoPaths!![0])
+            videoPlayer.setVideoURI(Uri.parse(videoPaths!![0]))
+            videoPlayer.start()
+
         }
     }
 }
